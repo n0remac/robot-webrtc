0b29acf250640239ad1dcb9b842eba11fc908bee
Cameron Smart <smartcameron@gmail.com>
2025-03-15 22:59:02 -0700

    file serving

--- main.go
+++ main.go
@@ -14,11 +14,11 @@ var clients = make(map[*websocket.Conn]bool)
 var broadcast = make(chan Message)
 
 type Message struct {
-    Type      string      `json:"type"`
-    Name      string      `json:"name,omitempty"`
-    Offer     interface{} `json:"offer,omitempty"`
-    Answer    interface{} `json:"answer,omitempty"`
-    Candidate interface{} `json:"candidate,omitempty"`
+	Type      string      `json:"type"`
+	Name      string      `json:"name,omitempty"`
+	Offer     interface{} `json:"offer,omitempty"`
+	Answer    interface{} `json:"answer,omitempty"`
+	Candidate interface{} `json:"candidate,omitempty"`
 }
 
 // Define constants and variables
@@ -32,10 +32,16 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
-	// Setup HTTP server
-	http.HandleFunc("/ws", handleWebSocket)
+	// Serve static files from the 'web' directory
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
+
+	// Explicitly serve JS and CSS
+	http.Handle("/app.js", http.FileServer(http.Dir("./web")))
+	http.Handle("/style.css", http.FileServer(http.Dir("./web")))
+
+	// Handle WebSockets separately
+	http.HandleFunc("/ws", handleWebSocket)
 
 	fmt.Printf("Starting server at http://localhost%s\n", webPort)
 	log.Fatal(http.ListenAndServe(webPort, nil))
@@ -43,41 +49,41 @@ func main() {
 
 // Handles WebSocket messages and broadcasts them
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println(err)
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
 
-    clients[conn] = true
+	clients[conn] = true
 
-    for {
-        _, message, err := conn.ReadMessage()
-        if err != nil {
-            log.Println("Error reading message:", err)
-            delete(clients, conn)
-            break
-        }
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			delete(clients, conn)
+			break
+		}
 
-        var msg Message
-        if err := json.Unmarshal(message, &msg); err != nil {
-            log.Println("Error unmarshaling message:", err)
-            continue
-        }
+		var msg Message
+		if err := json.Unmarshal(message, &msg); err != nil {
+			log.Println("Error unmarshaling message:", err)
+			continue
+		}
 
-        // Broadcast the message to all clients except sender
-        for client := range clients {
-            if client != conn {
-                err := client.WriteJSON(msg)
-                if err != nil {
-                    log.Println("Error writing JSON:", err)
-                    client.Close()
-                    delete(clients, client)
-                }
-            }
-        }
-    }
+		// Broadcast the message to all clients except sender
+		for client := range clients {
+			if client != conn {
+				err := client.WriteJSON(msg)
+				if err != nil {
+					log.Println("Error writing JSON:", err)
+					client.Close()
+					delete(clients, client)
+				}
+			}
+		}
+	}
 }
 
 func createPeerConnection() (*webrtc.PeerConnection, error) {
